ecc1d3dbbd0df87fb0bc85ca69d11dac7e2968a0
RenanGAS <renansakaoki@gmail.com>
2022-04-19 10:28:44 -0300

    Updates

---
+++ precache-manifest.3f708417c08983a1227e96a9f9a92140.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "60dd9728ec9f3476a48b",
+    "url": "/linktree-rrsc/static/css/main.9daef7d0.chunk.css"
+  },
+  {
+    "revision": "60dd9728ec9f3476a48b",
+    "url": "/linktree-rrsc/static/js/main.60dd9728.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/linktree-rrsc/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "413b728f6bda178a29fd",
+    "url": "/linktree-rrsc/static/js/2.413b728f.chunk.js"
+  },
+  {
+    "revision": "4c57c6101cb2fc213b3e",
+    "url": "/linktree-rrsc/static/js/runtime~main.4c57c610.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/linktree-rrsc/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "b1a693ef3ef3192f0641825218271d8d",
+    "url": "/linktree-rrsc/index.html"
+  }
+];
