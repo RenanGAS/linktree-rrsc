7c9902952bd9e5e2a1279d64121373df8d83fb9b
RenanGAS <renansakaoki@gmail.com>
2022-04-30 15:32:21 -0400

    chore: delete the components and bring back App.css to src

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import './css/App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-       
-      <Footer />
     </div>
   );
 }
