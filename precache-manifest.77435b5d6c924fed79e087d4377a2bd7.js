072ccb7134725a550120ef713320281a72f597ab
RenanGAS <renansakaoki@gmail.com>
2022-04-30 16:17:04 -0400

    Updates

--- precache-manifest.77435b5d6c924fed79e087d4377a2bd7.js
+++ precache-manifest.77435b5d6c924fed79e087d4377a2bd7.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "20cb4370d67eea9d8b99",
+    "revision": "b8a730819562f3188e71",
     "url": "/linktree-rrsc/static/css/main.9e58262b.chunk.css"
   },
   {
-    "revision": "20cb4370d67eea9d8b99",
-    "url": "/linktree-rrsc/static/js/main.20cb4370.chunk.js"
+    "revision": "b8a730819562f3188e71",
+    "url": "/linktree-rrsc/static/js/main.b8a73081.chunk.js"
   },
   {
     "revision": "2d25b52d749ad82315e7",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/linktree-rrsc/static/js/runtime~main.53ddb160.js"
   },
   {
-    "revision": "ddde8e5854c89b00892ad711bc815873",
+    "revision": "c91f5f8572dfc4b9a1131dd7ed0fbb03",
     "url": "/linktree-rrsc/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/linktree-rrsc/precache-manifest.1be7d391d9cb40467de588849dee9bc3.js"
+  "/linktree-rrsc/precache-manifest.77435b5d6c924fed79e087d4377a2bd7.js"
 );
 
 workbox.clientsClaim();
