be357f25c067b4ddadfedeb1eb957e3b23557de0
RenanGAS <renansakaoki@gmail.com>
2022-04-30 16:21:16 -0400

    feat: start the dark and light mode feature

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
+import './scripts/switchThemeButton.js';
 
 function App() {
   return (
     <div className="App">
+      <label class="switchBtn" for="checkbox">
+        <input type="checkbox" id="checkbox">
+          <span class="slider round"></span>
+        </input>
+      </label>
     </div>
   );
 }
---
+++ src/scripts/switchThemeButton.js
@@ -0,0 +1,12 @@
+const switchButton = document.querySelector('.switchBtn input[type="checkbox"]');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }    
+}
+
+switchButton.addEventListener('change', switchTheme, false);
