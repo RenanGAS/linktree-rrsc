cebe57381b354822abc904b4cfa63831b073afb2
RenanGAS <renansakaoki@gmail.com>
2022-05-01 05:15:14 -0400

    feat: add the Switch Theme Button and the Logo Image

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './App.css';
-import './scripts/switchThemeButton.js';
+import '../scripts/switchThemeButton.js';
+import logo from '../assets/logo_rr.png';
 
 function App() {
   return (
     <div className="App">
-      <label class="switchBtn" for="checkbox">
-        <input type="checkbox" id="checkbox">
-          <span class="slider round"></span>
-        </input>
-      </label>
+      <nav>
+        <div className="theme-switch-wrapper">
+          <label className="theme-switch" for="checkbox">
+            <input type="checkbox" id="checkbox" />
+            <div className="slider round"></div>
+          </label>
+        </div>
+      </nav>
+      <img className="logo" src={logo} alt="RR Smart Cast"/>
     </div>
   );
 }
