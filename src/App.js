ad627d2655400d7ffdc44003cd94b2ef0209bfcc
RenanGAS <renansakaoki@gmail.com>
2022-05-01 15:35:46 -0400

    fix: try to fix the elements' position in the page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container, Navbar } from 'reactstrap';
 import './App.css';
 // import './scripts/switchThemeButton.js';
 import logo from './assets/logo_rr.png';
@@ -6,15 +7,22 @@ import logo from './assets/logo_rr.png';
 function App() {
   return (
     <div class="App">
-      <nav>
-        <div class="theme-switch-wrapper">
-          <label class="theme-switch" for="checkbox">
-            <input type="checkbox" id="checkbox"/>
-            <div class="slider round"></div>
-          </label>
-        </div>
-      </nav>
-      <img class="logo" src={logo} alt="RR Smart Cast"/>
+
+      <Navbar fixed="top" bg="light" expand="lg">
+        <Container>
+          <div class="theme-switch-wrapper">
+            <label class="theme-switch" for="checkbox">
+              <input type="checkbox" id="checkbox" />
+              <div class="slider round"></div>
+            </label>
+          </div>
+        </Container>
+      </Navbar>
+      
+      <Container>
+        <img class="logo" src={logo} alt="RR Smart Cast" />
+      </Container>
+
     </div>
   );
 }
