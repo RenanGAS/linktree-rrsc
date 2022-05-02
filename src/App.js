ca49f51713652a46cb9388a0622ad2762d11b1fa
RenanGAS <renansakaoki@gmail.com>
2022-05-02 10:53:13 -0400

    chore: put the page content right next to the navbar

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
 import React from 'react';
 import { Container, Navbar } from 'reactstrap';
 import './App.css';
-// import './scripts/switchThemeButton.js';
 import logo from './assets/logo_rr.png';
 
 function App() {
   return (
-    <div class="App">
+    <div className="App">
 
-      <Navbar fixed="top" bg="light" expand="lg">
+      <Navbar className="justify-content-end" fixed="top" bg="light" expand="lg">
         <Container>
-          <div class="theme-switch-wrapper">
-            <label class="theme-switch" for="checkbox">
+          <div className="theme-switch-wrapper">
+            <label className="theme-switch" for="checkbox">
               <input type="checkbox" id="checkbox" />
-              <div class="slider round"></div>
+              <div className="slider round"></div>
             </label>
           </div>
         </Container>
       </Navbar>
-      
-      <Container>
-        <img class="logo" src={logo} alt="RR Smart Cast" />
-      </Container>
 
-      <script src="./scripts/switchThemeButton.js"></script>
+      <div className="page-content">
+        <img className="logo" src={logo} alt="RR Smart Cast" />
+        <h3 className="title">R&R SmartCast</h3>
+        <p className="subtitle">Building the future through code</p>
+      </div>
 
     </div>
   );
