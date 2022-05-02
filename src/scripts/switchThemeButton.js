52aa98016e0ac39468c77d9295530a7dfc8d9b8e
RenanGAS <renansakaoki@gmail.com>
2022-05-02 10:52:37 -0400

    to fix: import correctly the script file

--- src/scripts/switchThemeButton.js
+++ src/scripts/switchThemeButton.js
@@ -1,13 +1,13 @@
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
+toggleSwitch.addEventListener('change', switchTheme, false);
+
 function switchTheme(e) {
     if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
+        document.documentElement.setAttribute('data-theme', 'dark', true);
         localStorage.setItem('theme', 'dark');
     }
     else {
-        document.documentElement.setAttribute('data-theme', 'light');
+        document.documentElement.setAttribute('data-theme', 'light', true);
     }
 }
-
-toggleSwitch.addEventListener('change', switchTheme, false);
