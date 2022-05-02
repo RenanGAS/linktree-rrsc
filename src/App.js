c5b6c6a6577a629fe31bef14498d2d0cdd872403
RenanGAS <renansakaoki@gmail.com>
2022-05-02 16:48:21 -0400

    fix: correct the size of the buttons and set the links and icons

--- src/App.js
+++ src/App.js
@@ -23,11 +23,11 @@ function App() {
         <img className="logo" src={logo} alt="RR Smart Cast" />
         <h3 className="title">R&R SmartCast</h3>
         <p className="subtitle">Building the future through code</p>
-        <Button className="social-buttons"> <img src={facebook} className="img-fluid"/> </Button>{' '}
-        <Button className="social-buttons"> <img src={facebook} className="img-fluid"/> </Button>{' '}
-        <Button className="social-buttons"> <img src={facebook} className="img-fluid"/> </Button>{' '}
-        <Button className="social-buttons"> <img src={facebook} className="img-fluid"/> </Button>{' '}
-        <Button className="social-buttons"> <img src={facebook} className="img-fluid"/> </Button>
+        <Button className="social-buttons fb-button" href="https://www.facebook.com/rrsmartcast" target="_blank"></Button>{' '}
+        <Button className="social-buttons tw-button" href="https://twitter.com/SmartcastR" target="_blank"></Button>{' '}
+        <Button className="social-buttons inst-button" href="https://www.instagram.com/rrsccompany/" target="_blank"></Button>{' '}
+        <Button className="social-buttons em-button" href="mailto:renansakaoki@gmail.com" target="_blank"></Button>{' '}
+        <Button className="social-buttons gh-button" href="https://github.com/RRSCCompany" target="_blank"></Button>
       </div>
 
     </div>
