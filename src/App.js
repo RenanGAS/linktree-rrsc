f2a8a0497f36fb40cc9c64225337a7f4445c4f79
RenanGAS <renansakaoki@gmail.com>
2022-05-04 12:32:30 -0400

    feat: add the Website Button and hold the Social Buttons with a div

--- src/App.js
+++ src/App.js
@@ -9,16 +9,23 @@ function App() {
     <div className="App">
 
       <SwitchButton />
-      
+
       <div className="page-content">
+
         <img className="logo" src={logo} alt="RR Smart Cast" />
+
         <h3 className="title">R&R SmartCast</h3>
         <p className="subtitle">Building the future through code</p>
-        <Button className="social-buttons fb-button" href="https://www.facebook.com/rrsmartcast" target="_blank"></Button>{' '}
-        <Button className="social-buttons tw-button" href="https://twitter.com/SmartcastR" target="_blank"></Button>{' '}
-        <Button className="social-buttons inst-button" href="https://www.instagram.com/rrsccompany/" target="_blank"></Button>{' '}
-        <Button className="social-buttons em-button" href="mailto:renansakaoki@gmail.com" target="_blank"></Button>{' '}
-        <Button className="social-buttons gh-button" href="https://github.com/RRSCCompany" target="_blank"></Button>
+
+        <div className="group-buttons">
+          <Button className="social-buttons fb-button" href="https://www.facebook.com/rrsmartcast" target="_blank"></Button>{' '}
+          <Button className="social-buttons tw-button" href="https://twitter.com/SmartcastR" target="_blank"></Button>{' '}
+          <Button className="social-buttons inst-button" href="https://www.instagram.com/rrsccompany/" target="_blank"></Button>{' '}
+          <Button className="social-buttons em-button" href="mailto:renansakaoki@gmail.com" target="_blank"></Button>{' '}
+          <Button className="social-buttons gh-button" href="https://github.com/RRSCCompany" target="_blank"></Button>
+        </div>
+
+        <Button className="web-button" href="https://rrsmartcast.com.br" target="_blank"> Go to the website </Button>
       </div>
 
     </div>
